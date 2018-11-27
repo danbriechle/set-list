91eedd71bca3bdc0e17b9812a03b14ad0220c850
Dione Wilson <dionew1@gmail.com>
2018-11-27 14:34:57 -0700

    Complete Passing Data Lesson

--- app/controllers/set_list_app.rb
+++ app/controllers/set_list_app.rb
@@ -3,4 +3,19 @@ class SetList < Sinatra::Base
     @songs = Song.all
     erb :"songs/index"
   end
+
+  get '/songs/new' do
+    erb :"songs/new"
+  end
+
+  post '/songs' do
+    Song.create(params[:song])
+    redirect "/songs"
+  end
+
+  get '/songs/:id' do
+    @song = Song.find(params[:id])
+    erb :"songs/show"
+  end
+
 end
